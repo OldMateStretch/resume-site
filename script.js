d94e5c51e60579df4b07751990b0a3610b75a6db
OldMateStretch <Cameronrallison@gmail.com>
2025-03-31 17:36:39 +0800

    Added theme persistence code

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const toggleButton = document.querySelector(".theme-toggle");
 const body = document.getElementById("site-body");
 
-if (localStorage.getItem("theme") === "dark") {
-  body.classList.add("dark");
-}
-
 toggleButton.addEventListener("click", () => {
   body.classList.toggle("dark");
   if (body.classList.contains("dark")) {
--- to-do-list/script.js
+++ to-do-list/script.js
@@ -78,3 +78,15 @@ function createTaskElement(taskObject) {
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+const toggleButton = document.querySelector(".theme-toggle");
+const body = document.getElementById("site-body");
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
