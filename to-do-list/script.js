e2102a497ff9d864e9ae296e3b2358d67b397e6a
OldMateStretch <Cameronrallison@gmail.com>
2025-03-31 17:00:34 +0800

    Remove old to-do-list folder in /projects

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,6 +1,8 @@
 const taskInput = document.getElementById("new-task");
 const addButton = document.getElementById("add-task");
+const clearButton = document.getElementById("clear-tasks");
 const taskList = document.getElementById("task-list");
+const themeToggle = document.getElementById("theme-toggle");
 
 let tasks = [];
 
@@ -31,6 +33,12 @@ addButton.addEventListener("click", () => {
       taskInput.classList.remove("input-error");
     }, 600);
   }
+});
+
+clearButton.addEventListener("click", () => {
+  tasks = [];
+  localStorage.removeItem("tasks");
+  taskList.innerHTML = "";
 });
 
 function createTaskElement(taskObject) {
@@ -67,10 +75,6 @@ function createTaskElement(taskObject) {
   taskList.appendChild(taskItem);
 }
 
-const clearButton = document.getElementById("clear-tasks");
-
-clearButton.addEventListener("click", () => {
-  tasks = [];
-  localStorage.removeItem("tasks");
-  taskList.innerHTML = "";
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
